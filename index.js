39ce17f1d439651c33b0c28da329908b8e4019e4
Nestor David Castillo <nestor.castillo@ieee.org>
2023-05-08 09:25:18 -0500

    Nuevo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let buffer;
 app.use(express.json());
 
 app.listen(PORT, ()=>{
@@ -13,9 +14,14 @@ app.get('/webhook',(req,res)=>{
     
     console.log(req.body)
 
+    buffer = req.body;
     res.status(200).send({
         "timestamp": req.body.timestamp,
         "data": req.body.data
     });
 
 });
+
+app.get('/datos', (req,res)=>{
+    res.status(200).send(buffer);
+})
